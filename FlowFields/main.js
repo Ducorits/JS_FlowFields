088878d9b497e1a3023c37baced53e6b525f246e
Duco Ritsema van Eck <duco.ritsema@am-flow.com>
2024-08-28 13:17:17 +0200

    Sick looks

--- FlowFields/main.js
+++ FlowFields/main.js
@@ -13,12 +13,24 @@ class Particle {
 		this.effect = effect;
 		this.x = Math.floor(Math.random() * this.effect.width);
 		this.y = Math.floor(Math.random() * this.effect.height);
+		this.speedX = 0.5;
+		this.speedY = 1;
 	}
 	draw(context){
-		context.fillRect(this.x, this.y, 10, 10);
-
+		context.fillRect(this.x, this.y, 20, 20);
 	}
-
+	update(){
+		this.x += this.speedX;
+		this.y += this.speedY;
+		if (this.x > this.effect.width)
+			this.x = 0;
+		if (this.y > this.effect.height)
+			this.y = 0;
+		if (this.x < 0)
+			this.x = this.effect.width;
+		if (this.y < 0)
+			this.y = this.effect.height;
+	}
 }
 
 class Effect {
@@ -26,7 +38,8 @@ class Effect {
 		this.width = width;
 		this.height = height;
 		this.particles = [];
-		this.numberOfParticles = 50;
+		this.numberOfParticles = 80;
+		this.init();
 	}
 	init(){
 		// create particles
@@ -36,13 +49,22 @@ class Effect {
 		}
 	}
 	render(context){
+		context.fillStyle = 'rgba(0, 0, 0, 0.025)';
+		context.fillRect(0, 0, this.width, this.height);
+		context.fillStyle = 'white';
 		this.particles.forEach(particle => {
 			particle.draw(context);
+			particle.update();
 		});
 	}
 }
 
 const effect = new Effect(canvas.width, canvas.height);
-effect.init();
-effect.render(ctx);
 console.log(effect);
+
+function animate()
+{
+	effect.render(ctx);
+	requestAnimationFrame(animate);
+}
+animate();
